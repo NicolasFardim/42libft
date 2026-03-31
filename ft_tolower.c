37cd6ffb69d416af2e636830d741527a50600bae
Nicolas <nicolasfardim@gmail.com>
2026-03-31 19:39:27 +0100

    simplified

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,15 +1,8 @@
 #include "libft.h"
 
-static int	ft_isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
-}
-
 int	ft_tolower(int c)
 {
-	if (ft_isupper(c))
+	if (c >= 'A' && c <= 'Z')
 		c += 32;
 	return (c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -1,15 +1,8 @@
 #include "libft.h"
 
-static int	ft_islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	return (0);
-}
-
 int	ft_toupper(int c)
 {
-	if (ft_islower(c))
+	if (c >= 'a' && c <= 'z')
 		c -= 32;
 	return (c);
 }
